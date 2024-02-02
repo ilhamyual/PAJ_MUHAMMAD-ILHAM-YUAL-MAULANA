fc00021e28f96f25383de3136aab20361d7f5f2e
ilhamyual <82436226+ilhamyual@users.noreply.github.com>
2024-02-02 09:37:56 +0700

    git push

---
+++ Minggu1/resources/views/servis.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta naem="views" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Services</title>
+</head>
+<body>
+    <h1> Ini Halaman Services </h1>
+</body>
+</html>
