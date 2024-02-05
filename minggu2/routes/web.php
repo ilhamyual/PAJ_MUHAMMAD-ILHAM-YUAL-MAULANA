57b594eca2525cb5d995aeaa085746b822edefad
ilhamyual <82436226+ilhamyual@users.noreply.github.com>
2024-02-05 22:21:48 +0700

    git push

---
+++ minggu2/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VoterController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SubjectController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/voter', [VoterController::class, 'viewVoter']);
+
+Route::get('/subjectstudent', [SubjectController::class, 'viewsubjectstudent']);
+Route::get('/subjectteacher', [SubjectController::class, 'viewsubjectteacher']);
+Route::get('/subjectadmin', [SubjectController::class, 'viewsubjectadmin']);
+
+Route::get('/userstudent', [UserController::class, 'viewuserstudent']);
+Route::get('/userteacher', [UserController::class, 'viewuserteacher']);
+Route::get('/useradmin', [UserController::class, 'viewuseradmin']);
