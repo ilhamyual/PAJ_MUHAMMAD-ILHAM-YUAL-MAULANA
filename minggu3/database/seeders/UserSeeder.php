38e9cd466e15ed3469bec6e598cd15e40cb0a5b4
ilhamyual <82436226+ilhamyual@users.noreply.github.com>
2024-02-12 12:18:06 +0700

    git push

---
+++ minggu3/database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        \App\Models\Voter::factory(10)->create();
+
+        \App\Models\Voter::factory()->create([
+            'name'=> 'Test User',
+            'email'=> 'test@example.com',
+            'address'=> 'bondowoso',
+        ]);
+    }
+}
