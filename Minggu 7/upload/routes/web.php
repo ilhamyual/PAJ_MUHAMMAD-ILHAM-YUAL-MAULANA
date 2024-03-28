a90d633a98180f2a5568658a6203b34368e2391a
ilhamyual <82436226+ilhamyual@users.noreply.github.com>
2024-03-28 12:14:27 +0700

    upload

--- Minggu 7/upload/routes/web.php
+++ Minggu 7/upload/routes/web.php
@@ -2,9 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VoterController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SubjectController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\PatientController;
+use App\Http\Controllers\UploadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +21,10 @@ use App\Http\Controllers\PatientController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/upload', [UploadController::class, 'upload']);
+Route::post('/upload/proses', [UploadController::class, 'proses_upload']);
+Route::delete('/upload/hapus/{id}', [UploadController::class, 'hapus']);
 
 Route::middleware(['auth','is_admin'])->group(function(){
     Route::get('/admin/dashboard',[AdminController::class,'showDashboard'])->name('admin.dashboard');
@@ -44,3 +53,29 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+// Route::get('/task', [TaskController::class, 'index']);
+// Route::get('/task/create', [TaskController::class, 'create']);
+// Route::post('/task/store', [TaskController::class, 'store']);
+
+// Route::get('/task/{task}', [TaskController::class, 'show']);
+// Route::get('/task/{task}/edit', [TaskController::class, 'edit']);
+// Route::put('/task/{task}', [TaskController::class, 'update']);
+// Route::delete('/task/{task}' ,[TaskController::class, 'delete']);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/voter', [VoterController::class, 'viewVoter']);
+
+// Route::get('/subjectstudent', [SubjectController::class, 'viewsubjectstudent']);
+// Route::get('/subjectteacher', [SubjectController::class, 'viewsubjectteacher']);
+// Route::get('/subjectadmin', [SubjectController::class, 'viewsubjectadmin']);
+
+// Route::get('/userstudent', [UserController::class, 'viewuserstudent']);
+// Route::get('/userteacher', [UserController::class, 'viewuserteacher']);
+// Route::get('/useradmin', [UserController::class, 'viewuseradmin']);
+
+
+
